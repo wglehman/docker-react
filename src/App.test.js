7396ac0732c5abf5eded0f3e9404b7774a6318c1
Will Lehman <13912927+wglehman@users.noreply.github.com>
2021-10-14 15:23:24 -0400

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it("renders without crashingk", () => {
+  const div = document.createElement("div");
+  render(<App />, div);
+  unmountComponentAtNode(div);
 });
